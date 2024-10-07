07a412f36523a8738b62ff61289419d2b57b14c4
Basil Eagle <basileagle@gmail.com>
2024-10-07 17:30:31 +0100

    initial

---
+++ index.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('node:path');
+
+Menu.setApplicationMenu(null);
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+          preload: path.join(__dirname, 'preload.js')
+        }
+    });
+  
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+});
